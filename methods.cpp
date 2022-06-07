0c3423c97a9ea1bf6b322e7b87c28e8ba7d878d8
KingAlogon <milestbradley@gmail.com>
2022-06-07 01:32:01 +0000

    Eight Commit

--- methods.cpp
+++ methods.cpp
@@ -7,22 +7,24 @@ bool guessInList(string guess, string arr[], int num_words)
 {   
     int i;
     for(i = 0; i < num_words; i++)
+    {
         if(arr[i] == guess)
         {
             cout << "The word '" << arr[i] << "' exists!" << endl;
             return true;
         }
+    }
     return false;
 }
 
-int * compareWords(char guess_arr[], char answer_arr[])
+void compareWords(char guess_arr[], char answer_arr[], int score[])
 {
-    int score [] = {0, 0, 0, 0, 0};
-    int i;
-    for(i = 0; i < 5; i++)
+
+    for(int i = 0; i < 5; i++)
+    {
         bool isScored = false;
-        int j;
-        for(j = 0; j <5; j++)
+
+        for(int j = 0; j <5; j++)
         
             if(guess_arr[i] == answer_arr[j])
             {
@@ -44,13 +46,78 @@ int * compareWords(char guess_arr[], char answer_arr[])
         {
             score[i] = 0;
         }
-    return score;
+    }
+}
+// May need another argument here, a preexisting array of ___ size, which depends on the algorithm of this method.
+void cleanChArray(char guess_array[], int score[], char remainingChArray[], char newChArray[])
+{
+
+
+    int remaining_count = sizeof(remainingChArray);
+    int z = 0;
+    for(int i = 0; i < remaining_count; i+=5)
+    {
+        bool keep_word = true;
+        for(int j = 0; j < 5; j++)
+        {
+            char guess_ch = guess_array[j];
+            int score_j = score[j];
+            if(score_j == 0)
+            {
+                for(int k = 0; k < 5; k++)
+                {
+                    if(guess_ch == remainingChArray[i+k])
+                    {
+                        keep_word = false;
+                        break;
+                    }
+                }
+            }
+            else if(score_j == 2)
+            {
+                if(!(guess_ch == remainingChArray[i+j]))
+                {
+                    keep_word = false;
+                    break;
+                }
+            }
+            else if(score_j == 1)
+            {
+                for(int l = 0; l < 5; l++)
+                {
+                    if(!(guess_ch == remainingChArray[i+l] && j!=l))
+                    {
+                        keep_word = false;
+                        break;
+                    }
+                    else if(guess_ch == remainingChArray[i+l])
+                    {
+                        keep_word = false;
+                        break;
+                    }
+                }
+            }
+            if(!keep_word)
+            {
+                break;
+            }
+        }
+        if(keep_word)
+        {
+            for(int k = i; k < i+5; k++)
+            {
+                newChArray[z] = remainingChArray[k];
+                z++;
+            }
+        }
+    }
 }
 
 int main()
 {
     const int num_words = 2309;
     string ANSWERLIST[num_words];
+    char chArray[11545];
     int count = 0;
     int numberOfWords;
     
@@ -68,9 +135,37 @@ int main()
         count++;
     }
     numberOfWords = count;
-    
-    guessInList("trust", ANSWERLIST, numberOfWords);
+    char chAnswerList[11545];
+    for(int i = 0; i < numberOfWords; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            printf("%c ", ANSWERLIST[i][j]);
+            chAnswerList[i] = ANSWERLIST[i][j];
+        }
+        printf("\n");
+    }
 
+    int score_template[5] = {0, 0, 0, 0, 0};
+    
+    guessInList("pasta", ANSWERLIST, numberOfWords);
+    
+    char guess_arr[5] = {'s', 'c', 'o', 'p', 'e'};
+    
+    char answer_arr[5] = {'s', 'c', 'o', 'r', 'n'};
+    
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%i ", score_template[i]);
+    }
+    printf("\n");
+    
+    cleanChArray(guess_arr, score_template, chAnswerList, chArray);
+
+    for(int i = 0; i < 11545; i++)
+    {
+        printf("%c", chArray[i]);
+    }
     return 0;
 }
 
